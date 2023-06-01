997d4c620f1946007a97601639569de399efb2af
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-06-02 00:29:40 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
+const app: Application = express()
+
+//cors
+app.use(cors())
+
+//parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
+
+//Testing
+app.get('/', async (req: Request, res: any) => {
+  res.send('Working Successfully')
+})
+
+export default app
